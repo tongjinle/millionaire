e007d02c9ed01638f82d20f3291ab1d61a4227cd
shzjj82 <shzjj82@163.com>
2016-12-30 19:06:19 +0800

    344

--- src/box.js
+++ src/box.js
@@ -1,18 +1,25 @@
+var arr=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
 var Box = cc.Sprite.extend({
 	ctor:function(){
 		this._super();
 
-		this.speed = 2;
+		this.speed = 100;
 		this.letter = this._getRndLetter();
-
-		
-		// todo 加个字母
+		var label = cc.LabelTTF.create(this.letter, "Arial", 20);
+		this.addChild(label, 1);
 	},
 	move:function(dist){
 		this.y -= dist;
 	},
 	_getRndLetter:function(){
-		return 'A';
+		var num=Math.ceil(Math.random()*arr.length-1);
+		arr.splice(num,1);
+		console.log(arr);
+		if(arr.length==0){
+			arr=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+		}
+		return arr[num];
 	}
 
 });
